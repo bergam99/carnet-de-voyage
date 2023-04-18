f7867a3f4d86cacc37f9693209e1dde28f261fa1
Jiyun <grosjeanjiyun@icloud.com>
2023-04-18 16:31:23 +0200

    style

--- src/app/components/voyage-summery/voyage-summery.component.ts
+++ src/app/components/voyage-summery/voyage-summery.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Travel } from 'src/app/mock/travel-mock/travel.mock';
+import { ArticleService } from 'src/app/services/article/article.service';
 
 @Component({
   selector: 'app-voyage-summery',
   templateUrl: './voyage-summery.component.html',
-  styleUrls: ['./voyage-summery.component.css']
+  styleUrls: ['./voyage-summery.component.css'],
 })
 export class VoyageSummeryComponent {
+  @Input() emptyText: string='';
 
+  article?: Travel;
+  constructor(
+    private articleService: ArticleService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.initArticle();
+  }
+
+  initArticle() {
+    const id = Number(this.activatedRoute.snapshot.paramMap.get('id'));
+    const foundArticle = this.articleService.getArticle(id);
+    if (foundArticle) {
+      this.article = foundArticle;
+    }
+  }
 }
