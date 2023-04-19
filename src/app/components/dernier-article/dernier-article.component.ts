74f96e5aab08050bb284812afeffdf24df0bbf69
Jiyun <grosjeanjiyun@icloud.com>
2023-04-19 11:51:40 +0200

    voyager un peu plus

--- src/app/components/dernier-article/dernier-article.component.ts
+++ src/app/components/dernier-article/dernier-article.component.ts
@@ -1,23 +1,36 @@
-import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Component, Input } from '@angular/core';
 import { Travel } from 'src/app/mock/travel-mock/travel.mock';
 import { ArticleService } from 'src/app/services/article/article.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-dernier-article',
   templateUrl: './dernier-article.component.html',
-  styleUrls: ['./dernier-article.component.css']
+  styleUrls: ['./dernier-article.component.css'],
+  // date pipe
+  providers: [DatePipe],
 })
 export class DernierArticleComponent {
-  articles:Travel[]=[];
+  articles: Travel[] = [];
+  @Input() dateForDeparture: number = 0;
+  @Input() dateForArrival: number = 0;
 
-  constructor(private articleService : ArticleService){}
+  constructor(
+    private articleService: ArticleService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private datePipe: DatePipe
+  ) {}
 
-  ngOnInit(){
+  ngOnInit() {
     this.articles = this.getArticles();
   }
 
-  getArticles(){
+  getArticles() {
     return this.articleService.getArticles();
   }
+
+
 
 }
